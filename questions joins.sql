ab749676fea8513aba8617aacd0a911bea3292b4
sneha sahu <snehaasahu04@gmail.com>
2025-01-13 14:04:22 +0530

    Add files via upload

---
+++ questions joins.sql
@@ -0,0 +1,21 @@
+-- PRACTICE QUESTIONS
+
+-- 1. Fetch all orders along with customer names and order amounts.
+
+-- 2. Find details of orders and the corresponding products ordered.
+
+-- 3. Get all customers and their corresponding orders, if they have any.
+
+-- 4. List all products along with the order quantity, if they are ordered.
+
+-- 5. Retrieve all orders and include customer details if available.
+
+-- 6. List all customers and their orders. Include customers who haven't placed orders and orders without valid customers.
+
+-- 7. Find pairs of customers from the same country.
+
+-- 8. Retrieve order details, including customer name, product name, and total price (Quantity * Price).
+
+
+
+
